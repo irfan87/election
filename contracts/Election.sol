fc672a5ac9e88a41abab08f77bbede871c80dc09
irfan87 <nerve2009@yahoo.com>
2018-08-25 04:24:49 +0800

    creating fron-end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.24;
 
 contract Election {
+    // model the candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
     // store candidate
-    // read candidate
-    string public candidate;
-    // Constructor
+    // fetch candidate
+    mapping(uint => Candidate) public candidates;
+
+    // store candidates count
+    uint public candidatesCount;
 
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
